758f8ae1d8c305bc371b6c580e961d9bab4ed03b
Jeevan <gairejeevan7@gmail.com>
2024-08-08 12:42:04 +0530

    Build the ColorMode switch.

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ function App() {
       </GridItem>
       {/* render only in large screen */}
       <Show above="lg"> 
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>

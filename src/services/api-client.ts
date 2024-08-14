9174448d2260818ff96337f7526ef1747b779983
Jeevan <gairejeevan7@gmail.com>
2024-08-14 17:55:45 +0530

    Create a generic data fetching hook.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "fc03425806e943159b8ca09fd79b99e5",
+    key: "8b26c025472245efbd4d36dae215dd10",
   },
 });

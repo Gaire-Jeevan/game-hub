3158dfdb0986ae3e529c69d16438534793e14f58
Jeevan <gairejeevan7@gmail.com>
2024-08-15 11:36:56 +0530

    Displaying the genres.

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,17 @@ function App() {
         lg: `"nav     nav" 
             "aside    main"`, // wider than 1024px
       }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr'
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       {/* render only in large screen */}
       <Show above="lg"> 
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
